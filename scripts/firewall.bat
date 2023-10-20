be3a2a540d15da7a2c25f43b98e957953a0b0b2b
kdharvey25 <116278819+kdharvey25@users.noreply.github.com>
2023-10-20 16:18:42 -0400

    please

---
+++ scripts/firewall.bat
@@ -0,0 +1,13 @@
+echo Enabling firewall (make sure group policy is allowing modifications to the firewall)
+netsh advfirewall set allprofiles state on
+echo Firewall enabled
+echo Setting basic firewall rules..
+netsh advfirewall firewall set rule name="Remote Assistance (DCOM-In)" new enable=no 
+netsh advfirewall firewall set rule name="Remote Assistance (PNRP-In)" new enable=no 
+netsh advfirewall firewall set rule name="Remote Assistance (RA Server TCP-In)" new enable=no 
+netsh advfirewall firewall set rule name="Remote Assistance (SSDP TCP-In)" new enable=no 
+netsh advfirewall firewall set rule name="Remote Assistance (SSDP UDP-In)" new enable=no 
+netsh advfirewall firewall set rule name="Remote Assistance (TCP-In)" new enable=no 
+netsh advfirewall firewall set rule name="Telnet Server" new enable=no 
+netsh advfirewall firewall set rule name="netcat" new enable=no
+echo Set basic firewall rules 
