80de0c8faefcf76b7d6304b30cef376ca707d386
Kevin <kdharvey@flinthill.org>
2023-10-23 09:26:32 -0700

    asdf

--- manageUsers.bat
+++ manageUsers.bat
@@ -2,7 +2,7 @@
 REM Adds back all normal users
 FOR /F "tokens=* delims=" %%f IN (%currentPath%\users.txt) DO (
     net localgroup Administrators %%f /del
-	net user %%f 1qaz2wsx3edc4rfvA!
+	net user %%f 1NoPointDed! /add
 	WMIC USERACCOUNT WHERE Name='%%f' SET PasswordExpires=TRUE
 	net user %%f /active:yes
 	net user %%f /PasswordChg:yes
