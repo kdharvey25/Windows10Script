8e4ffc0a9330ca650bb18e08ae962333b36c24a3
kdharvey25 <116278819+kdharvey25@users.noreply.github.com>
2023-10-23 13:05:39 -0400

    adsf

--- path.ps1
+++ path.ps1
@@ -3,3 +3,19 @@ $parentDirectory = Split-Path -Path $PWD -Parent
 
 # Display the parent directory path
 Write-Host "Parent Directory: $parentDirectory"
+
+
+$filePath = "C:\Path\To\Your\File.txt"
+
+# Check if the file exists
+if (Test-Path $filePath) {
+    # If the file exists, delete it
+    Remove-Item $filePath -Force
+}
+
+# Create the file (it will be empty)
+New-Item -Path $filePath -ItemType File
+
+# You can also write some content to the file if needed
+# For example, to create a new file with content:
+# "This is the content" | Out-File -FilePath $filePath
