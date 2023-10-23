812ce328a494e7350774243dc6a82d3993471958
kdharvey25 <116278819+kdharvey25@users.noreply.github.com>
2023-10-23 12:20:02 -0400

    asdf

--- manageUsers.bat
+++ manageUsers.bat
@@ -1,3 +1,10 @@
-FOR /F "tokens=* delims=" %%i IN (%currentPath%\users.txt) DO (
-    
+
+REM Adds back all normal users
+FOR /F "tokens=* delims=" %%f IN (%currentPath%\users.txt) DO (
+    net localgroup Administrators %%f /del
+	net user %%f 1qaz2wsx3edc4rfvA!
+	WMIC USERACCOUNT WHERE Name='%%f' SET PasswordExpires=TRUE
+	net user %%f /active:yes
+	net user %%f /PasswordChg:yes
+	net user %%f /logonpasswordchg:yes
 )
