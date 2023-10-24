c47b64c0ec6939742bfe7bd319a0a0019ce93ed1
Kevin <kdharvey@flinthill.org>
2023-10-23 17:17:31 -0700

    fixed usrList.ps1

--- resources/usrList.ps1
+++ resources/usrList.ps1
@@ -1,12 +1,19 @@
+#Self elevate
+if(-NOT([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){   
+	$arguments="& '"+$myinvocation.mycommand.definition+"'"
+	Start-Process powershell -Verb runAs -ArgumentList $arguments
+	Break
+}
+
 $path=Split-Path -Path $PWD -Parent
-$path2=Get-content $path/output
-$accounts = Get-Wmiobject Win32_UserAccount -filter 'LocalAccount=TRUE' | select-object -expandproperty Name
+echo $path
+$path2=Get-item $path/output
 if (Test-Path $path2/currentUsers.txt) {
     Remove-Item $path2/currentUsers.txt -Force
 }
+$currentUsername=$env:UserName
 New-Item -Path $path2/currentUsers.txt -ItemType File
-$userList = Get-WmiObject -Class Win32_UserAccount | Where-Object { $_.Name -ne $currentUsername -and $_.Name -ne 'Administrator' }
-$userList | Select-Object Name, FullName | Export-Csv -Path $path2/currentUsers.txt -NoTypeInformation
-foreach($l in $accounts){
+$userList = Get-WmiObject -Class Win32_UserAccount | Where-Object { $_.Name -ne $currentUsername -and $_.Name -ne 'Administrator' } | select-object -expandproperty Name
+foreach($l in $userList){
 	$l >> "$path2/currentUsers.txt"
 }
