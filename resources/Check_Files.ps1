6c2a379c6b108d4e93dca38f2c93e943de1c7015
kdharvey25 <116278819+kdharvey25@users.noreply.github.com>
2023-10-23 21:11:21 -0400

    asdf

--- resources/Check_Files.ps1
+++ resources/Check_Files.ps1
@@ -7,31 +7,32 @@ if(-NOT([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity
 $path2=Split-Path -Path $PWD -Parent
 $path=Get-item $path2/output
 
-# File extensions and tools to search for
-$extensions = @("aac", "ac3", "avi", "aiff", "bat", "bmp", "exe", "flac", "gif", "jpeg", "jpg", "mov", "m3u", "m4p",
-    "mp2", "mp3", "mp4", "mpeg4", "midi", "msi", "ogg", "png", "txt", "sh", "wav", "wma", "vqf")
-$tools = @("Cain", "nmap", "keylogger", "Armitage", "Wireshark", "Metasploit", "netcat")
-
 # Create a directory to store the search results if it doesn't exist
-$outputDirectory = Join-Path $path "checkFilesOutput"
+$outputDirectory = Join-Path $path2 "checkFilesOutput"
 if (-not (Test-Path $outputDirectory)) {
     New-Item -ItemType Directory -Path $outputDirectory
 }
 
 # Function to search for files by extension
 function SearchFilesByExtension($extension) {
-    $outputFile = Join-Path $outputDirectory "$extension.txt"
-    Get-ChildItem -Path C:\ -Filter "*.$extension" -Recurse | ForEach-Object {
-        $_.FullName
-    } | Out-File $outputFile
+    $files = Get-ChildItem -Path C:\ -Filter "*.$extension" -Recurse
+    if ($files.Count -gt 0) {
+        $outputFile = Join-Path $outputDirectory "$extension.txt"
+        $files | ForEach-Object {
+            $_.FullName
+        } | Out-File $outputFile
+    }
 }
 
 # Function to search for tools
 function SearchForTool($toolName) {
-    $outputFile = Join-Path $outputDirectory "$toolName.txt"
-    Get-ChildItem -Path C:\ -Filter "*$toolName*" -Recurse | ForEach-Object {
-        $_.FullName
-    } | Out-File $outputFile
+    $files = Get-ChildItem -Path C:\ -Filter "*$toolName*" -Recurse
+    if ($files.Count -gt 0) {
+        $outputFile = Join-Path $outputDirectory "$toolName.txt"
+        $files | ForEach-Object {
+            $_.FullName
+        } | Out-File $outputFile
+    }
 }
 
 # Search for files by extension
