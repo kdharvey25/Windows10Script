0427ef59491f140fc1b4be51bdc5d461216ee091
kdharvey25 <116278819+kdharvey25@users.noreply.github.com>
2023-10-25 09:47:33 -0400

    new way

---
+++ manageUsers.ps1
@@ -0,0 +1,58 @@
+
+$path2 = Get-Item .
+$path = Join-Path -path $path2 -ChildPath "users"
+# Define the file paths for the lists of approved admins and standard users
+$approvedAdminsFile = Join-Path -Path $path -ChildPath "admins"
+$approvedUsersFile = Join-Path -Path $path -ChildPath "users"
+
+# Get the current list of users on the computer
+$existingUsers = Get-WmiObject Win32_UserAccount | Where-Object { $_.LocalAccount -eq $true } | Where-Object { $_.Name -ne $currentUsername -and $_.Name -ne 'Administrator' }
+
+# Read the lists of approved admins and users from the text files
+$approvedAdmins = Get-Content -Path $approvedAdminsFile
+$approvedUsers = Get-Content -Path $approvedUsersFile
+
+# Remove users who are not in the approved lists
+foreach ($user in $existingUsers) {
+    if ($user.Name -ne "Administrator" -and $user.Name -notin $approvedAdmins -and $user.Name -notin $approvedUsers) {
+        Remove-WmiObject -InputObject $user
+    }
+}
+
+# Add users who should be standard users
+foreach ($user in $approvedUsers) {
+    if ($user -notin $existingUsers.Name) {
+        net user $user /add
+    }
+}
+
+# Add users who should be administrators
+foreach ($user in $approvedAdmins) {
+    if ($user -notin $existingUsers.Name) {
+        net user $user /add
+    }
+    Add-LocalGroupMember -Group "Administrators" -Member $user
+}
+
+# Ensure only approved admins are in the Administrator group
+foreach ($user in $existingUsers) {
+    if ($user.Name -in $approvedAdmins) {
+        if ($user.Name -notin (Get-LocalGroupMember -Group "Administrators").Name) {
+            Add-LocalGroupMember -Group "Administrators" -Member $user.Name
+        }
+    } else {
+        if ($user.Name -in (Get-LocalGroupMember -Group "Administrators").Name) {
+            Remove-LocalGroupMember -Group "Administrators" -Member $user.Name
+        }
+    }
+}
+
+# Change privileges of admins who are not approved to standard users
+foreach ($user in $existingUsers) {
+    if ($user.Name -notin $approvedAdmins) {
+        net localgroup Administrators $user.Name /delete
+    }
+}
+
+# Notify that the user management is complete
+Write-Host "User management complete."
